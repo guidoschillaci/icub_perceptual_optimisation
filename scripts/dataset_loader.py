742fc8365b3f76a8376654661b72699f6223b25b
Guido Schillaci <guido.schillaci@santannapisa.it>
2020-12-09 23:07:40 +0100

    OF threshold

--- scripts/dataset_loader.py
+++ scripts/dataset_loader.py
@@ -113,7 +113,7 @@ class DatasetLoader():
                     opt_flow_polar_normalised = np.zeros((self.parameters.get('image_size'), self.parameters.get('image_size'), 3))
                 flow = cv2.calcOpticalFlowFarneback(self.dataset_images_t[i], self.dataset_images_tp1[i], None, 0.5, 3, 15, 3, 5, 1.2, 0)
                 magnitude, ang = cv2.cartToPolar(flow[..., 0], flow[..., 1])
-
+                print('max OF ', np.max(np.asarray(magnitude).flatten()), ' min ',np.min(np.asarray(magnitude).flatten()) )
                 opt_flow_polar_normalised[..., 0] = cv2.normalize(magnitude,None,0,255,cv2.NORM_MINMAX,dtype=cv2.CV_32F)
                 if not self.parameters.get('opt_flow_only_magnitude'):
                     opt_flow_polar_normalised[..., 1] = np.cos(ang) # ensures this is within -1,1 and doesnt' jump from 0to 180
