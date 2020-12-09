98fde82c5500d79923844e97ec3de7488f668f5a
Guido Schillaci <guido.schillaci@santannapisa.it>
2020-12-09 15:56:17 +0100

    scrits

--- docker/jetson_clear_results.sh
+++ docker/jetson_clear_results.sh
@@ -1,7 +1,10 @@
 echo "Deleting Optical Flow dataset"
 rm datasets/dataset_optical_flow.npy
-echo "Deleting old results"
-rm results/*
-rm results/plots/*
-rm results/plots/gif/*
-rm results/models/*
+echo "Deleting old results and models"
+rm -r results
+echo "Creating results directories"
+mkdir results
+mkdir results/plots
+mkdir results/plots/gif
+mkdir results/models
+echo "done!"
