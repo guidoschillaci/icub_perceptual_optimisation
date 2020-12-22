4ddb85825325c5af7eceb99287f139d81256fc8d
Guido Schillaci <guido.schillaci@santannapisa.it>
2020-12-22 16:26:18 +0100

    tf split in loss

--- scripts/models.py
+++ scripts/models.py
@@ -434,8 +434,8 @@ class Models:
             #print('tensor shape ', tf.shape(y_true))
             partitions = range(4)
             # split  observatiosn and predictions
-            true_out, true_aux_visual, true_aux_proprio, true_aux_motor = tf.dynamic_partition(y_true, partitions, 4)
-            pred_out, pred_aux_visual, pred_aux_proprio, pred_aux_motor = tf.dynamic_partition(y_pred, partitions, 4)
+            true_out, true_aux_visual, true_aux_proprio, true_aux_motor = tf.split(y_true, 4, axis=1)
+            pred_out, pred_aux_visual, pred_aux_proprio, pred_aux_motor = tf.split(y_pred, 4, axis=1)
 
             alpha = 0.2
             beta = 0.1
