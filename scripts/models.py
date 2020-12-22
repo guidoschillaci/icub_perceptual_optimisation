bbc958e3d25f99cbf7a3ff3f19d6a6187cf31ff3
Guido Schillaci <guido.schillaci@santannapisa.it>
2020-12-22 11:58:55 +0100

    loss function, split y_pred

--- scripts/models.py
+++ scripts/models.py
@@ -431,8 +431,8 @@ class Models:
         def loss_aux(y_true, y_pred):
             print('tensor shape ', tf.shape(y_true))
             # split  observatiosn and predictions
-            true_out, true_aux_visual, true_aux_proprio, true_aux_motor, true_fus_w = tf.unstack(y_true, axis=0)
-            pred_out, pred_aux_visual, pred_aux_proprio, pred_aux_motor, pred_fus_w = tf.unstack(y_pred, axis=0)
+            true_out, true_aux_visual, true_aux_proprio, true_aux_motor = tf.split(y_true, axis=0)
+            pred_out, pred_aux_visual, pred_aux_proprio, pred_aux_motor = tf.split(y_pred, axis=0)
 
             alpha = 0.2
             beta = 0.1
