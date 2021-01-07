0b0fdabde2a2314d0b32b0691a40c64e4667cd41
Guido Schillaci <guido.schillaci@santannapisa.it>
2021-01-07 15:01:13 +0100

    custom loss

--- docker/jetson_run.sh
+++ docker/jetson_run.sh
@@ -1,3 +1,7 @@
+echo "pulling most recent version"
+git pull
 echo "Clearing previous results"
 sh docker/jetson_clear_results.sh
+echo "Running main script"
 python3 scripts/main.py
+echo "Run script done!"
