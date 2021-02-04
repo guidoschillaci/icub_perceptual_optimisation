1078e9a405e0fd4e40000090dd75cd5b9fc0850f
Guido Schillaci <guido.schillaci@santannapisa.it>
2021-02-04 15:49:55 +0100

    keras fit

--- scripts/parameters.py
+++ scripts/parameters.py
@@ -26,7 +26,7 @@ class Parameters:
             'test_dataset_factor': 0.1, # the portion of the train dataset to be used as test
             'use_skin_data': False,
 
-            'model_custom_training_loop': True, # if False, use standard keras fit functions
+            'model_custom_training_loop': False, # if False, use standard keras fit functions
             'model_auxiliary': False, # use auxiliary weight model
             'model_batch_size': 32,
             'model_epochs': 5,
