c4c2b04aafc82269d9e1a1205ac4065768069b09
Guido Schillaci <guido.schillaci@santannapisa.it>
2021-02-05 14:49:41 +0100

    attenuation

--- scripts/utils.py
+++ scripts/utils.py
@@ -25,6 +25,7 @@ def sensory_attenuation(predicted_opt_flow, next_image, background_image):
     unnorm_pred = (1.0 - predicted_opt_flow)*255
     print('unnomr shape ', unnorm_pred.shape)
     print('next_image shape ', next_image.shape)
+    print('background_image shape ', background_image.shape)
     # convert grayscale img to 3-channles + alpha
     attenuated_image = cv2.merge((next_image,next_image,next_image,unnorm_pred))
     return np.uint8(cv2.addWeighted(background_image, 255.0, attenuated_image, 255.0, 0.0))
