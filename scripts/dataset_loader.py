c7637c1145b653d4ec7255a3ccfd5e45f49b0d59
Guido Schillaci <guido.schillaci@santannapisa.it>
2021-02-05 18:06:29 +0100

    attenuation

--- scripts/dataset_loader.py
+++ scripts/dataset_loader.py
@@ -56,7 +56,7 @@ class DatasetLoader():
             self.dataset_images_t = []
             self.background_image = cv2.cvtColor(self.dataset_images_t_orig[0], cv2.COLOR_GRAY2RGBA)
             self.background_image = cv2.resize(self.background_image, (self.parameters.get('image_size'), self.parameters.get('image_size')), interpolation=cv2.INTER_LINEAR)
-            self.background_image[:, :, 3] = 255*np.ones(self.dataset_images_t_orig[0].shape) # alpha channel
+            self.background_image[:, :, 3] = 255*np.ones((self.parameters.get('image_size'), self.parameters.get('image_size'))) # alpha channel
 
             cv2.imwrite( self.parameters.get('directory_plots')+'background_image.png',  self.background_image)
             for i in tqdm(range(len(self.dataset_images_t_orig)-1)):
