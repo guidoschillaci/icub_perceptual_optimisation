b535041c22ea1560c0497a4dbda9c107df61d8b8
Guido Schillaci <guido.schillaci@santannapisa.it>
2021-02-10 14:19:40 +0100

    debugging loss fn

--- scripts/utils.py
+++ scripts/utils.py
@@ -53,7 +53,7 @@ class MyCallback(Callback):
         #self.logs = {}
 
     def on_train_begin(self, logs={}):
-        #self.history = {'loss': [], 'val_loss': []}
+        self.history = {'loss': [], 'val_loss': []}
         # sub model with fusion weights output
         self.model_fusion_weights = Model(inputs=self.model.input,
                                           outputs=self.model.get_layer(name='fusion_weights').output)
