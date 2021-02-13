41158120e72c9acd8a58fa4dbaecaca783e9056d
Guido Schillaci <guido.schillaci@santannapisa.it>
2021-02-13 08:09:16 +0100

    activity regularizer

--- scripts/models.py
+++ scripts/models.py
@@ -210,8 +210,8 @@ class FusionActivityRegularizationLayer(Layer):
         # repeat elements -> shape: [batch_size, image_shape_0, image_shape_1]
         #weight = tf.tile(x, [1, self.parameters.get('model_num_modalities')])
         #fact_matrix = tf.math.scalar_mul(fact, tf.ones_like(weight))
-        sig_soft_loss_aux = tf.nn.softmax(tf.math.sigmoid(tf.math.exp(-tf.math.pow(input, 2))))
-        #sig_soft_loss_aux = tf.math.sigmoid(tf.math.exp(-tf.math.pow(input, 2)))
+        #sig_soft_loss_aux = tf.nn.softmax(tf.math.sigmoid(tf.math.exp(-tf.math.pow(input, 2))))
+        sig_soft_loss_aux = tf.math.sigmoid(tf.math.exp(-tf.math.pow(input, 2)))
         #return fact_matrix * tf.math.pow((w - sig_soft_loss_aux), 2)
         return fact * tf.math.pow((w - sig_soft_loss_aux), 2)
 
