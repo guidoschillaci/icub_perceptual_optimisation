d22946f53f5bdd7775916ef40b8ff4e0fe5c0f78
Guido Schillaci <guido.schillaci@santannapisa.it>
2021-02-15 09:49:18 +0100

    activity regularizer

--- scripts/models.py
+++ scripts/models.py
@@ -348,7 +348,7 @@ class Models:
             pre_fusion_weight_visual, pre_fusion_weight_proprio, pre_fusion_weight_motor = Split()(fusion_weight_layer)
             fusion_weight_visual = FusionActivityRegularizationLayer(param=self.parameters, \
                                                   name='fusion_activity_regularizer_layer') \
-                ([pre_fusion_weight_visual, pre_fusion_weight_proprio, pre_fusion_weight_motor])
+                ((pre_fusion_weight_visual, pre_fusion_weight_proprio, pre_fusion_weight_motor))
         else:
             fusion_weight_visual, fusion_weight_proprio, fusion_weight_motor = Split()(fusion_weight_layer)
 
