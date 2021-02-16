3fa150390851a167faabb5bddfcbfd7dc238e775
Guido Schillaci <guido.schillaci@santannapisa.it>
2021-02-16 16:21:44 +0100

    mean op outside function bug

--- scripts/models.py
+++ scripts/models.py
@@ -209,9 +209,9 @@ class FusionActivityRegularizationLayer(Layer):
 
         self.reg_fact = tf.fill([self.parameters.get('model_batch_size')], 0.33)
         self.beta = self.parameters.get('model_sensor_fusion_beta')
-        self.loss = None
-        self.inputs = None
-        self.outputs = None
+        #self.loss = None
+        #self.inputs = None
+        #self.outputs = None
 
     def get_config(self):
         base_config = super(FusionActivityRegularizationLayer, self).get_config()
