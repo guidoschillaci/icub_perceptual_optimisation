c1ae82c7457537746f4f39a0ac70bbf890c0da5d
Guido Schillaci <guido.schillaci@santannapisa.it>
2021-02-25 11:45:35 +0100

    sim aruco markers

--- scripts/utils_sim.py
+++ scripts/utils_sim.py
@@ -1,9 +1,17 @@
 import cv2
+import numpy as np
 
-def generate_aruco_markers(int num):
+def generate_aruco_markers(num):
     print('generating ', str(num), ' aruco markers')
-
-
+    # load the ArUCo dictionary
+    arucoDict = cv2.aruco.Dictionary_get('DICT_4X4_50')
+    marker_size = 100
+    for i in range(num):
+        marker = np.zeros((marker_size, marker_size, 1), dtype="uint8")
+        cv2.aruco.drawMarker(arucoDict, i, marker_size, marker, 1)
+        # write the generated ArUCo tag to disk and then display it to our
+        # screen
+        cv2.imwrite('yarp/modules/marker'+i, marker)
 
 if __name__ == "__main__":
     generate_aruco_markers(9)
