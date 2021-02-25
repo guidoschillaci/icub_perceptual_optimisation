0644df29b4cb6914c6e9d39e0856a0627182dfad
Guido Schillaci <guido.schillaci@santannapisa.it>
2021-02-25 13:07:59 +0100

    aruco

--- scripts/utils_sim.py
+++ scripts/utils_sim.py
@@ -5,7 +5,7 @@ print('test')
 def generate_aruco_markers(num):
     print('generating ', str(num), ' aruco markers')
     # load the ArUCo dictionary
-    arucoDict = cv2.aruco.Dictionary_get('DICT_4X4_50')
+    arucoDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_50)
     marker_size = 100
     for i in range(num):
         marker = np.zeros((marker_size, marker_size, 1), dtype="uint8")
