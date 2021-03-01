1fd106cb715c5f4bc20b9d8713436913292dcf04
Guido Schillaci <guido.schillaci@santannapisa.it>
2021-03-01 09:49:02 +0100

    download experiments

--- docker/jetson_download_results.sh
+++ docker/jetson_download_results.sh
@@ -1,5 +1,5 @@
 echo "Clearing previous results"
 sh docker/jetson_clear_results.sh
 echo "Downloading from jetson"
-scp -r jetson@192.168.1.155:/home/jetson/code/icub_perceptual_optimisation/results ./
+scp -r jetson@192.168.1.155:/home/jetson/code/icub_perceptual_optimisation/experiments ./
 echo "Results and plots downloaded!"
