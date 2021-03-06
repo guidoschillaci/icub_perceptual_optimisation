8f2603b87d4fae516e4ec3fdf928034de2fc7a28
Guido Schillaci <guido.schillaci@santannapisa.it>
2021-03-06 13:30:39 +0100

    debugging

--- docker/jetson_clear_results.sh
+++ docker/jetson_clear_results.sh
@@ -1,5 +1,6 @@
 echo "Deleting Optical Flow dataset"
-rm datasets/dataset_optical_flow.npy
+rm datasets/icub_alone/dataset_optical_flow.npy
+rm datasets/icub_and_ball/dataset_optical_flow.npy
 echo "Deleting old results and models"
 rm -r experiments/
 echo "done!"
