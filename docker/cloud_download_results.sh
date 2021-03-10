9a1da4f6650ffb775d72556b96fe4fe62eb312ba
Guido Schillaci <guido.schillaci@santannapisa.it>
2021-03-10 12:38:55 +0100

    download results

---
+++ docker/cloud_download_results.sh
@@ -0,0 +1,16 @@
+ipclient=94.156.202.240
+
+if [ -z "$1" ]
+  then
+    echo "No argument supplied. I set the address of the cloud instance to localhost."
+  else
+    ipclient=$1
+    echo "Setting the address of the cloud instance to ${ipclient}"
+fi
+
+
+echo "Clearing previous results"
+sh scripts/clear_results.sh
+echo "Downloading from jetson"
+scp -r cloudsigma@${ipclient}:/home/jetson/code/icub_perceptual_optimisation/experiments ./
+echo "Results and plots downloaded!"
