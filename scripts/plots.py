f26878c0f2cc77129cc6075a5db0f2980d4b06ba
Guido Schillaci <guido.schillaci@santannapisa.it>
2021-03-11 14:26:45 +0100

    dcker plots

--- scripts/plots.py
+++ scripts/plots.py
@@ -6,17 +6,18 @@ import pandas as pd
 from tqdm import tqdm
 import os
 
-import Tkinter
+import tkinter
 if os.environ.get('DISPLAY','') == '':
     print('no display found. Using :0.0')
     os.environ.__setitem__('DISPLAY', ':0.0')
 
 
-def make_figure(means, stddevs, title, xlabel, ylabel):
+def make_figure(means, stddevs, title, xlabel, ylabel, y_lim):
     fig1 = plt.figure(figsize=(10, 10))
     plt.title(title)
     plt.ylabel(xlabel)
     plt.xlabel(ylabel)
+    plt.ylim(y_lim)
     plt.errorbar(range(len(means)), means, stddevs, capsize=5)#, errorevery=499)
     filename = title +'.jpg'
     plt.savefig(filename)
@@ -28,27 +29,71 @@ def do_stats_plot(num_runs):
     data_loss = []
     data_val_loss = []
     data_iou = []
+    data_mkr_orig = []
+    data_mkr_att = []
+    data_mkr_att_custom_0 = []
+    data_mkr_att_custom_1 = []
+    data_mkr_att_custom_2 = []
+    data_mkr_att_custom_3 = []
+    data_mkr_att_custom_4 = []
+    data_mkr_att_custom_5 = []
     # load results for each run of this experiment
     for run in range(num_runs):
         directory = 'run_' + str(run) + '/'
         data_loss.append(np.loadtxt(directory + 'plots/loss.txt'))
         data_val_loss.append(np.loadtxt(directory + 'plots/val_loss.txt'))
         data_iou.append(np.loadtxt(directory + 'plots/IoU.txt'))
+
+        data_mkr_orig.append(np.loadtxt(directory + 'plots/markers_in_original_img.txt'))
+        data_mkr_att.append(np.loadtxt(directory + 'plots/markers_in_attenuated_img.txt'))
+        data_mkr_att_custom_0.append(np.loadtxt(directory + 'plots/markers_in_att_custom_weig_0.txt'))
+        data_mkr_att_custom_1.append(np.loadtxt(directory + 'plots/markers_in_att_custom_weig_1.txt'))
+        data_mkr_att_custom_2.append(np.loadtxt(directory + 'plots/markers_in_att_custom_weig_2.txt'))
+        data_mkr_att_custom_3.append(np.loadtxt(directory + 'plots/markers_in_att_custom_weig_3.txt'))
+        data_mkr_att_custom_4.append(np.loadtxt(directory + 'plots/markers_in_att_custom_weig_4.txt'))
+        data_mkr_att_custom_5.append(np.loadtxt(directory + 'plots/markers_in_att_custom_weig_5.txt'))
     mean_loss = np.mean(np.asarray(data_loss), axis=0)
     mean_val_loss = np.mean(np.asarray(data_val_loss), axis=0)
     mean_iou = np.mean(np.asarray(data_iou), axis=0)
+
+    mean_mkr_orig = np.mean(np.asarray(data_mkr_orig), axis=0)
+    mean_mkr_att = np.mean(np.asarray(data_mkr_att), axis=0)
+    mean_mkr_att_custom_0 = np.mean(np.asarray(data_mkr_att_custom_0), axis=0)
+    mean_mkr_att_custom_1 = np.mean(np.asarray(data_mkr_att_custom_1), axis=0)
+    mean_mkr_att_custom_2 = np.mean(np.asarray(data_mkr_att_custom_2), axis=0)
+    mean_mkr_att_custom_3 = np.mean(np.asarray(data_mkr_att_custom_3), axis=0)
+    mean_mkr_att_custom_4 = np.mean(np.asarray(data_mkr_att_custom_4), axis=0)
+    mean_mkr_att_custom_5 = np.mean(np.asarray(data_mkr_att_custom_5), axis=0)
 
     stddev_loss = np.std(np.asarray(data_loss), axis=0)
     stddev_val_loss = np.std(np.asarray(data_val_loss), axis=0)
     stddev_iou = np.std(np.asarray(data_iou), axis=0)
 
-    make_figure(mean_loss, stddev_loss, 'Mean_Loss', 'loss', 'epoch')
-    make_figure(mean_val_loss, stddev_val_loss, 'Mean_Val_Loss', 'val_loss', 'epoch')
-    make_figure(mean_iou, stddev_iou, 'Mean_Intersection_Over_Unit', 'IoU', 'epoch')
+    stddev_mkr_orig = np.std(np.asarray(data_mkr_orig), axis=0)
+    stddev_mkr_att = np.std(np.asarray(data_mkr_att), axis=0)
+    stddev_mkr_att_custom_0 = np.std(np.asarray(data_mkr_att_custom_0), axis=0)
+    stddev_mkr_att_custom_1 = np.std(np.asarray(data_mkr_att_custom_1), axis=0)
+    stddev_mkr_att_custom_2 = np.std(np.asarray(data_mkr_att_custom_2), axis=0)
+    stddev_mkr_att_custom_3 = np.std(np.asarray(data_mkr_att_custom_3), axis=0)
+    stddev_mkr_att_custom_4 = np.std(np.asarray(data_mkr_att_custom_4), axis=0)
+    stddev_mkr_att_custom_5 = np.std(np.asarray(data_mkr_att_custom_5), axis=0)
+
+    make_figure(mean_loss, stddev_loss, 'Mean_Loss', 'loss', 'epoch', [0,1])
+    make_figure(mean_val_loss, stddev_val_loss, 'Mean_Val_Loss', 'val_loss', 'epoch',[0,1])
+    make_figure(mean_iou, stddev_iou, 'Mean_Intersection_Over_Unit', 'IoU', 'epoch',[0,1])
+
+    make_figure(mean_mkr_orig, stddev_mkr_orig, 'Mean_Marker_Detection_in_Original_Img', 'Markers detected', 'epoch', [6,9])
+    make_figure(mean_mkr_att, stddev_mkr_att, 'Mean_Marker_Detection_in_Attenuated_Img', 'Markers detected', 'epoch', [6,9])
+    make_figure(mean_mkr_att_custom_0, stddev_mkr_att_custom_0, 'Mean_Marker_Detection_in_Cust_Weight_0', 'Markers detected', 'epoch', [6,9])
+    make_figure(mean_mkr_att_custom_1, stddev_mkr_att_custom_1, 'Mean_Marker_Detection_in_Cust_Weight_1', 'Markers detected', 'epoch', [6,9])
+    make_figure(mean_mkr_att_custom_2, stddev_mkr_att_custom_2, 'Mean_Marker_Detection_in_Cust_Weight_2', 'Markers detected', 'epoch', [6,9])
+    make_figure(mean_mkr_att_custom_3, stddev_mkr_att_custom_3, 'Mean_Marker_Detection_in_Cust_Weight_3', 'Markers detected', 'epoch', [6,9])
+    make_figure(mean_mkr_att_custom_4, stddev_mkr_att_custom_4, 'Mean_Marker_Detection_in_Cust_Weight_4', 'Markers detected', 'epoch', [6,9])
+    make_figure(mean_mkr_att_custom_5, stddev_mkr_att_custom_5, 'Mean_Marker_Detection_in_Cust_Weight_5', 'Markers detected', 'epoch', [6,9])
 
 
 if __name__ == "__main__":
-    num_experiments = 6
+    num_experiments = 4
     num_runs = 5
     main_path = os.getcwd()
     multiple_experiments_folder = main_path + '/' + 'experiments_good'
