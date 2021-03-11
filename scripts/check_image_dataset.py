0a655ec99a54bcf0edd6deaf7cfc3c23e1dd1821
Guido Schillaci <guido.schillaci@santannapisa.it>
2021-03-11 10:26:41 +0100

    config

--- scripts/check_image_dataset.py
+++ scripts/check_image_dataset.py
@@ -4,9 +4,10 @@ import numpy as np
 def load_and_save(dataset):
     image_raw = np.load(dataset)
     # dataset.images_raw = np.load(folder + 'dataset_images.npy')
-    print('image raw size', image_raw.shape)
-    cv2.imshow('image_raw', image_raw[0])
-    cv2.imwrite('image_raw.png', image_raw[0])
+
+    print('dataset size size', image_raw.shape)
+    #cv2.imshow('image_raw', image_raw[0])
+    #cv2.imwrite('image_raw.png', image_raw[0])
 
 if __name__ == "__main__":
-    load_and_save('results/dataset_images_grayscale.npy')
+    load_and_save('datasets/icub_alone/dataset_images_grayscale.npy')
