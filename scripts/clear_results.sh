cadb0f41594a0f02bd8170d02fa7dbb1f0d47997
Guido Schillaci <guido.schillaci@santannapisa.it>
2021-04-03 11:11:05 +0200

    script

--- scripts/clear_results.sh
+++ scripts/clear_results.sh
@@ -1,6 +1,7 @@
 echo "Deleting Optical Flow dataset"
 rm datasets/icub_alone/dataset_optical_flow.npy
 rm datasets/icub_and_ball/dataset_optical_flow.npy
+rm datasets/icub_and_many_balls/dataset_optical_flow.npy
 echo "Deleting old results and models"
 rm -r experiments/
 echo "done!"
