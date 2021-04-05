6884578f202f293628f04da3d58a3b58e2412adb
Guido Schillaci <guido.schillaci@santannapisa.it>
2021-04-05 14:59:41 +0200

    docker gpu cloud

--- docker/cloud_gpu_setup.sh
+++ docker/cloud_gpu_setup.sh
@@ -23,13 +23,13 @@ sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
 
 # install nvidia drivers and cuda
-#sudo apt install -y linux-headers-$(uname -r)
-#sudo curl -O https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
-#sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
-#sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/7fa2af80.pub
-#sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
-#sudo apt update
-#sudo apt -y install cuda
+sudo apt install -y linux-headers-$(uname -r)
+sudo curl -O https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
+sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/7fa2af80.pub
+sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
+sudo apt update
+sudo apt -y install cuda
 
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
    && curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add - \
