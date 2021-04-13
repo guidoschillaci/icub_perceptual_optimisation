f5521a91c9ecb7a9423e0d9a09283aff15614f1d
Guido Schillaci <guido.schillaci@santannapisa.it>
2021-04-13 16:59:20 +0200

    iouplots

--- scripts/plots.py
+++ scripts/plots.py
@@ -328,11 +328,277 @@ def make_figure_markers(means_mo, stddevs_mo, \
     np.savetxt(title+'_p.txt', ["%s" % tabulate.tabulate(_table_p, headers, tablefmt="latex")], fmt='%s')
 
 
+def make_figure_iou(means_ma, stddevs_ma, \
+                        means_w0, stddevs_w0, \
+                        means_w1, stddevs_w1, \
+                        means_w2, stddevs_w2, \
+                        means_w3, stddevs_w3, \
+                        means_w4, stddevs_w4, \
+                        means_w5, stddevs_w5, \
+                        title, xlabel, ylabel, ylim):
+    fig1 = plt.figure(figsize=(10, 10))
+    plt.title(title)
+    plt.ylabel(xlabel)
+    plt.xlabel(ylabel)
+    plt.ylim(ylim)
+    plt.errorbar(range(len(means_ma)), means_ma, stddevs_ma, capsize=5, errorevery=3, label='main model')
+    plt.errorbar(range(len(means_w0)), means_w0, stddevs_w0, capsize=5, errorevery=3, label='custom_w 0')
+    plt.errorbar(range(len(means_w1)), means_w1, stddevs_w1, capsize=5, errorevery=3, label='custom_w 1')
+    plt.errorbar(range(len(means_w2)), means_w2, stddevs_w2, capsize=5, errorevery=3, label='custom_w 2')
+    plt.errorbar(range(len(means_w3)), means_w3, stddevs_w3, capsize=5, errorevery=3, label='custom_w 3')
+    plt.errorbar(range(len(means_w4)), means_w4, stddevs_w4, capsize=5, errorevery=3, label='custom_w 4')
+    plt.errorbar(range(len(means_w5)), means_w5, stddevs_w5, capsize=5, errorevery=3, label='custom_w 5')
+    plt.legend(ncol=2, loc='lower right')
+    filename = title + '.jpg'
+    plt.savefig(filename)
+    plt.close()
+
+    _table_stats = []
+    _table_p = []
+    _line_s = []
+    _line_p = []
+
+    _line_s.append('MA')
+    _line_p.append('MA')
+    stat, p = stats.ttest_ind(means_ma, means_mo)
+    _line_s.append(round(stat, 1))
+    _line_p.append(round(p,4))
+    # ma ma
+    _line_s.append('-')
+    _line_p.append('-')
+    stat, p = stats.ttest_ind(means_ma, means_w0)
+    _line_s.append(round(stat, 1))
+    _line_p.append(round(p,4))
+    stat, p = stats.ttest_ind(means_ma, means_w1)
+    _line_s.append(round(stat, 1))
+    _line_p.append(round(p,4))
+    stat, p = stats.ttest_ind(means_ma, means_w2)
+    _line_s.append(round(stat, 1))
+    _line_p.append(round(p,4))
+    stat, p = stats.ttest_ind(means_ma, means_w3)
+    _line_s.append(round(stat, 1))
+    _line_p.append(round(p,4))
+    stat, p = stats.ttest_ind(means_ma, means_w4)
+    _line_s.append(round(stat, 1))
+    _line_p.append(round(p,4))
+    stat, p = stats.ttest_ind(means_ma, means_w5)
+    _line_s.append(round(stat, 1))
+    _line_p.append(round(p,4))
+    _table_stats.append(_line_s)
+    _table_p.append(_line_p)
+    _line_s = []
+    _line_p = []
+
+    _line_s.append('W0')
+    _line_p.append('W0')
+    stat, p = stats.ttest_ind(means_w0, means_mo)
+    _line_s.append(round(stat, 1))
+    _line_p.append(round(p,4))
+    stat, p = stats.ttest_ind(means_w0, means_ma)
+    _line_s.append(round(stat, 1))
+    _line_p.append(round(p,4))
+    # w0 w0
+    _line_s.append('-')
+    _line_p.append('-')
+    stat, p = stats.ttest_ind(means_w0, means_w1)
+    _line_s.append(round(stat, 1))
+    _line_p.append(round(p,4))
+    stat, p = stats.ttest_ind(means_w0, means_w2)
+    _line_s.append(round(stat, 1))
+    _line_p.append(round(p,4))
+    stat, p = stats.ttest_ind(means_w0, means_w3)
+    _line_s.append(round(stat, 1))
+    _line_p.append(round(p,4))
+    stat, p = stats.ttest_ind(means_w0, means_w4)
+    _line_s.append(round(stat, 1))
+    _line_p.append(round(p,4))
+    stat, p = stats.ttest_ind(means_w0, means_w5)
+    _line_s.append(round(stat, 1))
+    _line_p.append(round(p,4))
+    _table_stats.append(_line_s)
+    _table_p.append(_line_p)
+    _line_s = []
+    _line_p = []
+
+
+    _line_s.append('W1')
+    _line_p.append('W1')
+    stat, p = stats.ttest_ind(means_w1, means_mo)
+    _line_s.append(round(stat, 1))
+    _line_p.append(round(p,4))
+    stat, p = stats.ttest_ind(means_w1, means_ma)
+    _line_s.append(round(stat, 1))
+    _line_p.append(round(p,4))
+    stat, p = stats.ttest_ind(means_w1, means_w0)
+    _line_s.append(round(stat, 1))
+    _line_p.append(round(p,4))
+    # w1 w1
+    _line_s.append('-')
+    _line_p.append('-')
+    stat, p = stats.ttest_ind(means_w1, means_w2)
+    _line_s.append(round(stat, 1))
+    _line_p.append(round(p,4))
+    stat, p = stats.ttest_ind(means_w1, means_w3)
+    _line_s.append(round(stat, 1))
+    _line_p.append(round(p,4))
+    stat, p = stats.ttest_ind(means_w1, means_w4)
+    _line_s.append(round(stat, 1))
+    _line_p.append(round(p,4))
+    stat, p = stats.ttest_ind(means_w1, means_w5)
+    _line_s.append(round(stat, 1))
+    _line_p.append(round(p,4))
+    _table_stats.append(_line_s)
+    _table_p.append(_line_p)
+    _line_s = []
+    _line_p = []
+
+
+    _line_s.append('W2')
+    _line_p.append('W2')
+    stat, p = stats.ttest_ind(means_w2, means_mo)
+    _line_s.append(round(stat, 1))
+    _line_p.append(round(p,4))
+    stat, p = stats.ttest_ind(means_w2, means_ma)
+    _line_s.append(round(stat, 1))
+    _line_p.append(round(p,4))
+    stat, p = stats.ttest_ind(means_w2, means_w0)
+    _line_s.append(round(stat, 1))
+    _line_p.append(round(p,4))
+    stat, p = stats.ttest_ind(means_w2, means_w1)
+    _line_s.append(round(stat, 1))
+    _line_p.append(round(p,4))
+    # w2 w2
+    _line_s.append('-')
+    _line_p.append('-')
+    stat, p = stats.ttest_ind(means_w2, means_w3)
+    _line_s.append(round(stat, 1))
+    _line_p.append(round(p,4))
+    stat, p = stats.ttest_ind(means_w2, means_w4)
+    _line_s.append(round(stat, 1))
+    _line_p.append(round(p,4))
+    stat, p = stats.ttest_ind(means_w2, means_w5)
+    _line_s.append(round(stat, 1))
+    _line_p.append(round(p,4))
+    _table_stats.append(_line_s)
+    _table_p.append(_line_p)
+    _line_s = []
+    _line_p = []
+
+
+    _line_s.append('W3')
+    _line_p.append('W3')
+    stat, p = stats.ttest_ind(means_w3, means_mo)
+    _line_s.append(round(stat, 1))
+    _line_p.append(round(p,4))
+    stat, p = stats.ttest_ind(means_w3, means_ma)
+    _line_s.append(round(stat, 1))
+    _line_p.append(round(p,4))
+    stat, p = stats.ttest_ind(means_w3, means_w0)
+    _line_s.append(round(stat, 1))
+    _line_p.append(round(p,4))
+    stat, p = stats.ttest_ind(means_w3, means_w1)
+    _line_s.append(round(stat, 1))
+    _line_p.append(round(p,4))
+    stat, p = stats.ttest_ind(means_w3, means_w2)
+    _line_s.append(round(stat, 1))
+    _line_p.append(round(p,4))
+    # w3 w3
+    _line_s.append('-')
+    _line_p.append('-')
+    stat, p = stats.ttest_ind(means_w3, means_w4)
+    _line_s.append(round(stat, 1))
+    _line_p.append(round(p,4))
+    stat, p = stats.ttest_ind(means_w3, means_w5)
+    _line_s.append(round(stat, 1))
+    _line_p.append(round(p,4))
+    _table_stats.append(_line_s)
+    _table_p.append(_line_p)
+    _line_s = []
+    _line_p = []
+
+
+    _line_s.append('W4')
+    _line_p.append('W4')
+    stat, p = stats.ttest_ind(means_w4, means_mo)
+    _line_s.append(round(stat, 1))
+    _line_p.append(round(p,4))
+    stat, p = stats.ttest_ind(means_w4, means_ma)
+    _line_s.append(round(stat, 1))
+    _line_p.append(round(p,4))
+    stat, p = stats.ttest_ind(means_w4, means_w0)
+    _line_s.append(round(stat, 1))
+    _line_p.append(round(p,4))
+    stat, p = stats.ttest_ind(means_w4, means_w1)
+    _line_s.append(round(stat, 1))
+    _line_p.append(round(p,4))
+    stat, p = stats.ttest_ind(means_w4, means_w2)
+    _line_s.append(round(stat, 1))
+    _line_p.append(round(p,4))
+    stat, p = stats.ttest_ind(means_w4, means_w3)
+    _line_s.append(round(stat, 1))
+    _line_p.append(round(p,4))
+    # w4 w4
+    _line_s.append('-')
+    _line_p.append('-')
+    stat, p = stats.ttest_ind(means_w4, means_w5)
+    _line_s.append(round(stat, 1))
+    _line_p.append(round(p,4))
+    _table_stats.append(_line_s)
+    _table_p.append(_line_p)
+    _line_s = []
+    _line_p = []
+
+
+    _line_s.append('W5')
+    _line_p.append('W5')
+    stat, p = stats.ttest_ind(means_w5, means_mo)
+    _line_s.append(round(stat, 1))
+    _line_p.append(round(p,4))
+    stat, p = stats.ttest_ind(means_w5, means_ma)
+    _line_s.append(round(stat, 1))
+    _line_p.append(round(p,4))
+    stat, p = stats.ttest_ind(means_w5, means_w0)
+    _line_s.append(round(stat, 1))
+    _line_p.append(round(p,4))
+    stat, p = stats.ttest_ind(means_w5, means_w1)
+    _line_s.append(round(stat, 1))
+    _line_p.append(round(p,4))
+    stat, p = stats.ttest_ind(means_w5, means_w2)
+    _line_s.append(round(stat, 1))
+    _line_p.append(round(p,4))
+    stat, p = stats.ttest_ind(means_w5, means_w3)
+    _line_s.append(round(stat, 1))
+    _line_p.append(round(p,4))
+    stat, p = stats.ttest_ind(means_w5, means_w4)
+    _line_s.append(round(stat, 1))
+    _line_p.append(round(p,4))
+    # w5 w5
+    _line_s.append('-')
+    _line_p.append('-')
+    _table_stats.append(_line_s)
+    _table_p.append(_line_p)
+    _line_s = []
+    _line_p = []
+
+
+    # print('Statistics=%.3f, p=%.3f' % (stat, p))
+    headers = ['',  'MA', 'W0', 'W1', 'W2', 'W3', 'W4', 'W5']
+    np.savetxt(title+'_stat.txt', ["%s" % tabulate.tabulate(_table_stats, headers, tablefmt="latex")], fmt='%s')
+    np.savetxt(title+'_p.txt', ["%s" % tabulate.tabulate(_table_p, headers, tablefmt="latex")], fmt='%s')
+
+
 # run this from the exp_x folder
 def do_stats_plot(num_runs,exp):
     data_loss = []
     data_val_loss = []
-    data_iou = []
+    data_iou_main = []
+    data_iou_custom_0 = []
+    data_iou_custom_1 = []
+    data_iou_custom_2 = []
+    data_iou_custom_3 = []
+    data_iou_custom_4 = []
+    data_iou_custom_5 = []
+
     data_mkr_orig = []
     data_mkr_att = []
     data_mkr_att_custom_0 = []
@@ -346,7 +612,14 @@ def do_stats_plot(num_runs,exp):
         directory = 'run_' + str(run) + '/'
         data_loss.append(np.loadtxt(directory + 'plots/loss.txt'))
         data_val_loss.append(np.loadtxt(directory + 'plots/val_loss.txt'))
-        data_iou.append(np.loadtxt(directory + 'plots/IoU.txt'))
+        data_iou_main.append(np.loadtxt(directory + 'plots/iou_main_model.txt'))
+
+        data_iou_custom_0.append(np.loadtxt(directory + 'plots/iou_model_with_custom_weights_0.txt'))
+        data_iou_custom_1.append(np.loadtxt(directory + 'plots/iou_model_with_custom_weights_1.txt'))
+        data_iou_custom_2.append(np.loadtxt(directory + 'plots/iou_model_with_custom_weights_2.txt'))
+        data_iou_custom_3.append(np.loadtxt(directory + 'plots/iou_model_with_custom_weights_3.txt'))
+        data_iou_custom_4.append(np.loadtxt(directory + 'plots/iou_model_with_custom_weights_4.txt'))
+        data_iou_custom_5.append(np.loadtxt(directory + 'plots/iou_model_with_custom_weights_5.txt'))
 
         data_mkr_orig.append(np.loadtxt(directory + 'plots/markers_in_original_img.txt'))
         data_mkr_att.append(np.loadtxt(directory + 'plots/markers_in_attenuated_img.txt'))
@@ -358,7 +631,13 @@ def do_stats_plot(num_runs,exp):
         data_mkr_att_custom_5.append(np.loadtxt(directory + 'plots/markers_in_att_custom_weig_5.txt'))
     mean_loss = np.mean(np.asarray(data_loss), axis=0)
     mean_val_loss = np.mean(np.asarray(data_val_loss), axis=0)
-    mean_iou = np.mean(np.asarray(data_iou), axis=0)
+    mean_iou_main = np.mean(np.asarray(data_iou_main), axis=0)
+    mean_iou_custom_0 = np.mean(np.asarray(data_iou_custom_0), axis=0)
+    mean_iou_custom_1 = np.mean(np.asarray(data_iou_custom_1), axis=0)
+    mean_iou_custom_2 = np.mean(np.asarray(data_iou_custom_2), axis=0)
+    mean_iou_custom_3 = np.mean(np.asarray(data_iou_custom_3), axis=0)
+    mean_iou_custom_4 = np.mean(np.asarray(data_iou_custom_4), axis=0)
+    mean_iou_custom_5 = np.mean(np.asarray(data_iou_custom_5), axis=0)
 
     mean_mkr_orig = np.mean(np.asarray(data_mkr_orig), axis=0)
     mean_mkr_att = np.mean(np.asarray(data_mkr_att), axis=0)
@@ -371,7 +650,14 @@ def do_stats_plot(num_runs,exp):
 
     stddev_loss = np.std(np.asarray(data_loss), axis=0)
     stddev_val_loss = np.std(np.asarray(data_val_loss), axis=0)
-    stddev_iou = np.std(np.asarray(data_iou), axis=0)
+
+    stddev_iou_main = np.std(np.asarray(data_iou_main), axis=0)
+    stddev_iou_custom_0 = np.std(np.asarray(data_iou_custom_0), axis=0)
+    stddev_iou_custom_1 = np.std(np.asarray(data_iou_custom_1), axis=0)
+    stddev_iou_custom_2 = np.std(np.asarray(data_iou_custom_2), axis=0)
+    stddev_iou_custom_3 = np.std(np.asarray(data_iou_custom_3), axis=0)
+    stddev_iou_custom_4 = np.std(np.asarray(data_iou_custom_4), axis=0)
+    stddev_iou_custom_5 = np.std(np.asarray(data_iou_custom_5), axis=0)
 
     stddev_mkr_orig = np.std(np.asarray(data_mkr_orig), axis=0)
     stddev_mkr_att = np.std(np.asarray(data_mkr_att), axis=0)
@@ -384,7 +670,18 @@ def do_stats_plot(num_runs,exp):
 
     make_figure_loss(mean_loss, stddev_loss,mean_val_loss, stddev_val_loss, 'exp'+str(exp)+'_Mean_Loss', 'loss', 'epoch', [0.00005,0.00022])
     #make_figure(, 'Mean_Val_Loss', 'val_loss', 'epoch',[0,1])
-    make_figure(mean_iou, stddev_iou, 'exp'+str(exp)+'_Mean_Intersection_Over_Unit', 'IoU', 'epoch',[0,1])
+    #make_figure(mean_iou, stddev_iou, 'exp'+str(exp)+'_Mean_Intersection_Over_Unit', 'IoU', 'epoch',[0,1])
+
+
+    make_figure_iou(mean_iou_main, stddev_iou_main, \
+                     mean_iou_custom_0, stddev_iou_custom_0, \
+                     mean_iou_custom_1, stddev_iou_custom_1, \
+                     mean_iou_custom_2, stddev_iou_custom_2, \
+                     mean_iou_custom_3, stddev_iou_custom_3, \
+                     mean_iou_custom_4, stddev_iou_custom_4, \
+                     mean_iou_custom_5, stddev_iou_custom_5, \
+                     'exp'+str(exp)+'_Mean_IoU', 'Intersection over Unit', 'epoch', [7,8.5])
+
 
     make_figure_markers(mean_mkr_orig, stddev_mkr_orig, \
                      mean_mkr_att, stddev_mkr_att, \
@@ -412,17 +709,17 @@ def do_ttest_self_other(num_runs):
     multiple_experiments_folder = main_path + '/' + 'experiments'
 
     for run in range(num_runs):
-        __self=np.loadtxt(multiple_experiments_folder+'/exp0/run_'+str(run)+'/plots/markers_in_attenuated_img.txt')
+        __self=np.loadtxt(multiple_experiments_folder+'/exp0/run_'+str(run)+'/plots/val_loss.txt')
         for i in range(10):
             _self.append(__self[i])
-        _other=np.loadtxt(multiple_experiments_folder+'/exp1/run_'+str(run)+'/plots/markers_in_attenuated_img.txt')
+        _other=np.loadtxt(multiple_experiments_folder+'/exp3/run_'+str(run)+'/plots/val_loss.txt')
         for i in range(10):
             other.append(_other[i])
     _self = np.asarray(_self)
     other = np.asarray(other)
     stat, p = stats.ttest_ind(_self, other)
-    print('self ', _self)
-    print('other ', _other)
+    #print('self ', _self)
+    #print('other ', other)
     print('self mean ', np.mean(_self), ' std ', np.std(_self))
     print('other mean ', np.mean(other), ' std ', np.std(other))
     print('stat ', stat, ' p ', p)
