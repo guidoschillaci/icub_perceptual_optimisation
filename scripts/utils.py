8aa03c13a6b72eeee293710b7ef8b34935bbc51d
Guido Schillaci <guido.schillaci@santannapisa.it>
2021-04-13 16:10:32 +0200

    tf binarise

--- scripts/utils.py
+++ scripts/utils.py
@@ -37,7 +37,7 @@ def binarize_optical_flow(param, optflow, positive_value = 255):
 # output image has values: 0 or positive_value
 def tf_binarize_optical_flow(param, optflow, positive_value = 255):
     if param.get('opt_flow_binarize'):
-        return np.array(tf.where(optflow > param.get('opt_flow_binary_threshold'), positive_value, 0), dtype='uint8')
+        return np.array(tf.where(tf.greater(optflow, param.get('opt_flow_binary_threshold')), positive_value, 0), dtype='uint8')
     return np.array(optflow, dtype='uint8')
 
 def intersection_over_union(param, y_true, y_pred):
