a7e4910effad866af0bc020e9efcb16483096b90
Guido Schillaci <guido.schillaci@santannapisa.it>
2021-04-13 16:24:00 +0200

    script

--- scripts/clear_results.sh
+++ scripts/clear_results.sh
@@ -4,11 +4,11 @@ rm datasets/only_ball/dataset_optical_flow.npy
 rm datasets/icub_and_ball/dataset_optical_flow.npy
 rm datasets/icub_and_many_balls/dataset_optical_flow.npy
 
-read -p "Do you want to remove the experiments folder? " -n 1 -r
-echo    # (optional) move to a new line
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
+#read -p "Do you want to remove the experiments folder? " -n 1 -r
+#echo    # (optional) move to a new line
+#if [[ $REPLY =~ ^[Yy]$ ]]
+#then
     echo "Deleting old results and models"
     rm -r experiments/
     echo "removed!"
-fi
+#fi
