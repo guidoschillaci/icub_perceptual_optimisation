87ca836e99ab250ffbe52a7ed0f5fd8779f7b40c
Guido Schillaci <guido.schillaci@santannapisa.it>
2021-04-13 16:24:38 +0200

    script

--- scripts/clear_results.sh
+++ scripts/clear_results.sh
@@ -3,6 +3,7 @@ rm datasets/icub_alone/dataset_optical_flow.npy
 rm datasets/only_ball/dataset_optical_flow.npy
 rm datasets/icub_and_ball/dataset_optical_flow.npy
 rm datasets/icub_and_many_balls/dataset_optical_flow.npy
+rm datasets/combined_alone_and_many_balls/dataset_optical_flow.npy
 
 #read -p "Do you want to remove the experiments folder? " -n 1 -r
 #echo    # (optional) move to a new line
