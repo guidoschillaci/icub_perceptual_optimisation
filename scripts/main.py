09e4d165bc584cfc86351caf640dd7da68b090b4
Guido Schillaci <guido.schillaci@santannapisa.it>
2021-04-18 14:05:24 +0200

    exp 1 and 4

--- scripts/main.py
+++ scripts/main.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     # DS 2: robot not moving and balls (1 every 20 sec); DS 3: robot moving and many balls (1 every sec)
     # DS 4: combined DS0 and DS3
     doe = build.build_full_fact( \
-        {'dataset_train_type': [0, 4], 'dataset_test_type': [3]})
+        {'dataset_train_type': [0, 1], 'dataset_test_type': [1]})
         #{'dataset_train_type': [0, 1, 2], 'dataset_test_type': [0, 1]})
     #    {'dataset_type': [0, 1], 'attenuation_test_dataset_type': [0, 1]})
     # add case:
